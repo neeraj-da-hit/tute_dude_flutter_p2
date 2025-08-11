5e2c7f457c075a64dc6e3410626e9e9bb23b524c
neeraj-da-hit <neerajdahit@gmail.com>
2025-08-11 18:06:20 +0530

    init commit

---
+++ card_connect/lib/home_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "Card Connect",
+          style: TextStyle(fontSize: 32, color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          height: 300,
+          padding: EdgeInsets.all(20),
+          color: Colors.blue[100],
+          child: Column(
+            spacing: 10,
+            children: [
+              Row(
+                spacing: 6,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.call),
+                  Text("+91 9045********", style: TextStyle(fontSize: 16)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                spacing: 10,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    color: Colors.grey[200],
+                    height: 120,
+                    child: Image(image: AssetImage('images/man_head.png')),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        spacing: 6,
+                        children: [
+                          Icon(Icons.person),
+                          Text(
+                            "Neeraj Dahit",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        spacing: 6,
+                        children: [
+                          Icon(Icons.home),
+                          Text(
+                            "Meerut City",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        spacing: 6,
+                        children: [
+                          Icon(Icons.work),
+                          Text(
+                            "Sakte Meerut UP",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                height: 2,
+                width: double.infinity,
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                padding: EdgeInsets.symmetric(horizontal: 20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.web, size: 16),
+                      Text("www.tutedude.com"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.email, size: 16),
+                      Text("email@gmail.com"),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
